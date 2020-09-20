8641f09c1468f61159464ee66bebc52ea426cf41
BurgBoi <davidwangshark@gmail.com>
2020-09-19 21:16:22 -0400

    Added the feature to create a new private text channel for a newly created event role for privacy.

--- src/main/java/me/david/tskmanager/Main.java
+++ src/main/java/me/david/tskmanager/Main.java
@@ -18,9 +18,9 @@ public class Main {
 	public static Color defaultEmbedColor = new Color(235, 183, 52);
 
 	public static void main(String[] args) throws LoginException {
-		JDABuilder jdaBuilder = JDABuilder.createDefault("NzU1ODU1MzE0ODA0NTM5NTE0.X2JXHg.a6pKpRYtA0YKLLPm5zbt2EJs_3s");
+		JDABuilder jdaBuilder = JDABuilder.createDefault("NzU1ODU1MzE0ODA0NTM5NTE0.X2JXHg.oBeVDw8vu0rBO1-yZUe2TRM6wk8");
 		jdaBuilder.setStatus(OnlineStatus.ONLINE);
-		jdaBuilder.setActivity(Activity.listening("to commands"));
+		jdaBuilder.setActivity(Activity.listening("commands"));
 		jdaBuilder.setMemberCachePolicy(MemberCachePolicy.ALL);
 		jdaBuilder.enableIntents(GatewayIntent.GUILD_MEMBERS);
 		addEventListeners(jdaBuilder);
@@ -29,6 +29,6 @@ public class Main {
 
 	private static void addEventListeners(JDABuilder jdaBuilder) {
 		jdaBuilder.addEventListeners(new HelpCommand(), new AddToFactionCommand(), new DefaultJoinRoles(), new EventChannels(), new HrRoleCommand(), new HrRolesCommand(), new LrRoleCommand(), new LrMrRolesCommand(), new PrefixCommand(),
-				new PromoteCommand(), new RanksTrackCommand(), new EventChannelsEventListener(), new AttendingEventRoleCommand(), new PrivateMessageCommand());
+				new PromoteCommand(), new RanksTrackCommand(), new EventChannelsEventListener(), new PrivateMessageCommand());
 	}
 }
