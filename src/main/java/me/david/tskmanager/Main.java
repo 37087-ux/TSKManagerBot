65c5ba2ee4ac3beff9ed6d9c853ad57074b41fbe
BurgBoi <davidwangshark@gmail.com>
2020-09-27 19:08:10 -0400

    Fixed a bug where it would create multiple event1s and added event commands.

--- src/main/java/me/david/tskmanager/Main.java
+++ src/main/java/me/david/tskmanager/Main.java
@@ -1,6 +1,9 @@
 package me.david.tskmanager;
 
 import me.david.tskmanager.commands.impl.*;
+import me.david.tskmanager.eventchannelcommands.impl.FinishEventCommand;
+import me.david.tskmanager.eventchannelcommands.impl.PrintLogsCommand;
+import me.david.tskmanager.eventchannelcommands.impl.RenameEventCommand;
 import me.david.tskmanager.eventlisteners.EventChannelsEventListener;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
@@ -18,7 +21,7 @@ public class Main {
 	public static Color defaultEmbedColor = new Color(235, 183, 52);
 
 	public static void main(String[] args) throws LoginException {
-		JDABuilder jdaBuilder = JDABuilder.createDefault("");
+		JDABuilder jdaBuilder = JDABuilder.createDefault("NzU1ODU1MzE0ODA0NTM5NTE0.X2JXHg.h4CaK96yOXt51LkZK1lvTos141w");
 		jdaBuilder.setStatus(OnlineStatus.ONLINE);
 		jdaBuilder.setActivity(Activity.listening("commands"));
 		jdaBuilder.setMemberCachePolicy(MemberCachePolicy.ALL);
@@ -29,6 +32,6 @@ public class Main {
 
 	private static void addEventListeners(JDABuilder jdaBuilder) {
 		jdaBuilder.addEventListeners(new HelpCommand(), new AddToFactionCommand(), new DefaultJoinRoles(), new EventChannels(), new HrRoleCommand(), new HrRolesCommand(), new LrRoleCommand(), new LrMrRolesCommand(), new PrefixCommand(),
-				new PromoteCommand(), new RanksTrackCommand(), new EventChannelsEventListener(), new PrivateMessageCommand(), new EventsCategoryCommand());
+				new PromoteCommand(), new RanksTrackCommand(), new EventChannelsEventListener(), new PrivateMessageCommand(), new EventsCategoryCommand(), new FinishEventCommand(), new PrintLogsCommand(), new RenameEventCommand());
 	}
 }
